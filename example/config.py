4d46001296ad083df6827a9c97333f0f093f31bd
Decklin Foster <decklin@red-bean.com>
2006-02-01 07:36:02 +0000

    Document new evil magic, and add required var.

--- example/config.py
+++ example/config.py
@@ -4,27 +4,26 @@
 # This file is a Python script. When run, the following variables will be
 # defined for you; you may change or add to them as you see fit.
 #
-# ``entries_dir``: a Maildir containing all the blog entries.
-# ``layout_dir``: the blog's layout, as a skeleton directory tree.
-# ``style_dir``: empy styles used for filling layout templates.
-# ``output_dir``: location where we will write the generated pages.
+# * ``entries_dir``: a Maildir containing all the blog entries.
+# * ``layout_dir``: the blog's layout, as a skeleton directory tree.
+# * ``style_dir``: empy styles used for filling layout templates.
+# * ``output_dir``: location where we will write the generated pages.
 #
 # These will be $HOME/Mnemosyne/{entries,layout,style,htdocs} respectively.
 #
-# ``vars``: a dict of default local variables passed to all templates.
+# * ``locals``: a dict of default local variables passed to all templates.
 #
 # This will contain the keys __version__, __url__, __author__, and __email__.
 #
-# You may also define functions here to add 'magic' attributes to each entry.
-# A function with a name of the form ``make_MAGIC`` (which takes a single
-# argument, the entry) will be used to create an attribute ``e._MAGIC`` for
-# each entry ``e``. Either a single value or a list of values may be returned.
+# * ``MnemosyneEntry``: a class used to represent each entry passed to the
+#   templates.
 #
-# In your layout, a file or directory name containing ``__MAGIC__`` will then
-# be evaluated once for each value ``make_MAGIC`` returns, with the entries
-# for which ``make_MAGIC`` returns that value or a list containing it.
+# If you wish to extend this class, you may define a new class ``Entry`` here,
+# using ``MnemosyneEntry`` as its base class. Any methods with a name of the
+# form ``get_ATTRIBUTE`` will be used to provide e.ATTRIBUTE at runtime.
 
-vars['blogname'] = 'Example Blog'
+locals['blogname'] = 'Example Blog'
+locals['base'] = 'http://example.invalid'
 
 class Entry:
     def get_organization(self):
