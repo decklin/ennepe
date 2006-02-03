a4744b3550a3415ea1ef6fbac0a4256dc742be4e
Decklin Foster <decklin@red-bean.com>
2006-02-03 03:03:05 +0000

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Mnemosyne',
+    version='0.1',
+    description='Static blog generation system',
+    author='Decklin Foster',
+    author_email='decklin@red-bean.com',
+    url='http://www.red-bean.com/~decklin/software/mnemosyne/',
+    classifiers=[
+        'Intended Audience :: Advanced End Users',
+        'Development Status :: 4 - Beta',
+        'License :: MIT/X Consortium License',
+        'Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+        'Operating System :: POSIX',
+        'Environment :: Console (Text Based)',
+        'Programming Language :: Python',
+        ],
+    package_dir = {'': 'lib'},
+    py_modules = ['mnemosyne', 'rsthtml'],
+    scripts = ['mnemosyne'],
+    data_files=[
+        # ('share/man1', ['mnemosyne.1', 'etc...']),
+        ],
+    )
