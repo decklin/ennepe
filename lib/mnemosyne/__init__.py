7ee401053d62bbf6c02c9bc836720979478aa1ea
Decklin Foster <decklin@red-bean.com>
2006-02-22 04:10:01 +0000

    Release 0.8. I didn't do docs, but the last thing was an actual bug.

--- lib/mnemosyne/__init__.py
+++ lib/mnemosyne/__init__.py
@@ -1,6 +1,6 @@
 """Mnemosyne - a weblog generator."""
 
-__version__ = '0.7'
+__version__ = '0.8'
 __author__ = 'Decklin Foster'
 __email__ = 'decklin@red-bean.com'
 __url__ = 'http://www.red-bean.com/~decklin/software/mnemosyne/'
